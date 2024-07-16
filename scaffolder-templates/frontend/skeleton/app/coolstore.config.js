9f363422c81e17239024839cb926272223db869d
varodrig <varodrig@redhat.com>
2024-07-16 11:53:17 -0400

    only vm templates

--- scaffolder-templates/frontend/skeleton/app/coolstore.config.js
+++
@@ -1,2 +0,0 @@
-angular.module("app")
-.constant("COOLSTORE_CONFIG", {"API_ENDPOINT":"gateway-vertx-undefined","SECURE_API_ENDPOINT":"gateway-vertx-undefined","SSO_ENABLED":false,"PRODUCT_REFRESH_INTERVAL":5000});
